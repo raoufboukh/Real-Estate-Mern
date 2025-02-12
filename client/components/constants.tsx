2363e5df79f788575dd25f4ee5c4916e5d526b00
raoufboukh <zakib2316@gmail.com>
2025-02-12 15:22:53 +0100

    ...

--- client/components/constants.tsx
+++ client/components/constants.tsx
@@ -24,92 +24,6 @@ export const links = [
   },
 ];
 
-export const residencies = [
-  {
-    id: 1,
-    name: "Aliva Priva Jardin",
-    image: "/assets/r1.png",
-    description:
-      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
-    country: "Indonesia",
-    city: "Jakarta",
-    address:
-      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
-    bedrooms: 5,
-    parkings: 2,
-    bathrooms: 3,
-    price: "47,043",
-  },
-  {
-    id: 2,
-    name: "Assati Garden City",
-    image: "/assets/r2.png",
-    description:
-      "Pahlawan Street XVII NO.215, Cinangka, Sawangan, Depok, Jawa Barat",
-    country: "Indonesia",
-    city: "Depok",
-    address:
-      "Pahlawan Street XVII NO.215, Cinangka, Sawangan, Depok, Jawa Barat",
-    bedrooms: 7,
-    parkings: 3,
-    bathrooms: 4,
-    price: "66,353",
-  },
-  {
-    id: 3,
-    name: "Citralan Puri Serang",
-    image: "/assets/r3.png",
-    description:
-      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
-    country: "Indonesia",
-    city: "Serang",
-    address:
-      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
-    bedrooms: 4,
-    parkings: 1,
-    bathrooms: 2,
-    price: "35,853",
-  },
-  {
-    id: 4,
-    name: "Aliva Priva Jardin",
-    image: "/assets/r1.png",
-    description:
-      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
-    country: "Indonesia",
-    city: "Jakarta",
-    address:
-      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
-    bedrooms: 5,
-    parkings: 2,
-    bathrooms: 3,
-    price: "47,043",
-  },
-  {
-    id: 5,
-    name: "Assati Garden City",
-    image: "/assets/r2.png",
-    description:
-      "Pahlawan Street XVII NO.215, Cinangka, Sawangan, Depok, Jawa Barat",
-    country: "Indonesia",
-    city: "Depok",
-    address:
-      "Pahlawan Street XVII NO.215, Cinangka, Sawangan, Depok, Jawa Barat",
-    bedrooms: 7,
-    parkings: 3,
-    bathrooms: 4,
-    price: "66,353",
-  },
-  {
-    id: 6,
-    name: "Citralan Puri Serang",
-    image: "/assets/r3.png",
-    description:
-      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
-    price: "35,853",
-  },
-];
-
 export const companies = [
   {
     image: "/assets/prologis.png",
@@ -169,7 +83,6 @@ export const contacts = [
     btn: "Message Now",
   },
 ];
-
 
 export const AddProp = [
   {
