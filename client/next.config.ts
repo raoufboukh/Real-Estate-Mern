f82e21be4e6ebb14b8ea511874e3219236c4a8e0
raoufboukh <zakib2316@gmail.com>
2025-02-12 23:48:34 +0100

    modification

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {};
+module.exports = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
